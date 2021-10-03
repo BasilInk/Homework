561728d86f2607d307894bf3e535c23e775e21a8
Stas <gomel.itstep@mail.com>
2021-10-03 10:07:22 +0300

    inplemented 2 tasks

---
+++ Conditionals/script.js
@@ -0,0 +1,36 @@
+//1. пользователь вводит 2 числа, вывести в консоль большее из них
+
+//2. пользователь вводит 3 числа, первые два это числа которые он хочет перемножить
+// третье число, это результат умножения по мнению пользователя
+// выяснить правильно ли ответил пользователь и вывести в консоль ОК если правильно
+// NOT OK {rightAnswer} если неправильно
+
+// Задача 1
+
+// let num1 = prompt ('Введите первое число!');
+// let num2 = prompt ('Введите второе число!');
+
+// if (num1 > num2) {
+//     console.log ("Первое число больше");
+// } else if (num1 === null || num2 === null) {
+//     console.log ("Вы ничего не ввели");
+// } else if (num1 === num2) {
+//     console.log ("Числа равны");
+// } else {
+//     console.log ("Второе число больше")
+// }
+
+// Задача 2
+
+// const num1 = +prompt('enter num1');
+// const num2 = +prompt('enter num2');
+// const userResult = +prompt('enter result');
+
+// const rightAnswer = num1 * num2;
+
+// if (userResult === rightAnswer) {
+//     console.log('OK');
+// } else {
+//     console.log(`NOT OK ${rightAnswer}`);
+// }
+
