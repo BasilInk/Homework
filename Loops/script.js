cbd5cb864b9cda9488ae288b9881361df9a7fb3f
Stas <gomel.itstep@mail.com>
2021-10-06 19:21:03 +0300

    3 task

--- Loops/script.js
+++ Loops/script.js
@@ -13,3 +13,30 @@
 // «Положительных: 2 шт.
 // Отрицательных: 5 шт.
 // Нулей: 3 шт.»
+
+let numbersAmount = +prompt('how many nums do you wanna enter ?');
+
+let zeros = 0;
+let biggerThanZero = 0;
+let lessThanZero = 0;
+
+while ( numbersAmount > 0 ) {
+    numbersAmount--;
+
+    const userNum = +prompt('enter a num');
+
+    if (userNum > 0) {
+        biggerThanZero++;
+    } else if ( userNum < 0) {
+        lessThanZero++;
+    } else {
+        zeros++
+    }
+}
+
+console.log(`
+    amount of zeros: ${zeros} 
+    amount bigger than zeros: ${biggerThanZero}
+    amount lesser than zeros: ${lessThanZero}
+`);
+
