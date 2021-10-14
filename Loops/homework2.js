cd04e28355ca1b2627e4e51ae29de973b0c8c1d4
BasilInk <vasiltsov_s_a@mail.ru>
2021-10-14 16:14:35 +0300

    ArraysHomework

--- Loops/homework2.js
+++ Loops/homework2.js
@@ -1,65 +1,116 @@
-// 1. Написать программу, которая выводит среднее
+// Задача 1.
+
+// Написать программу, которая выводит среднее
 // арифметическое n чисел введенных пользователем.
 // Ввод чисел прекращается
 // тогда, когда нажали «отмена», после чего выводится
 // среднее арифметическое.
 
-// let itog = 1;
-// let n = +prompt (`Enter a number!`);
-// for (i = 1; n !== null || n !== 0; i++) {
-//     +prompt (`Enter a number!`, ``);
-//     itog = 
-//     console.log (itog);
-// }
+// Задача 1. Решение 1.
 
-// 2. Пользователь вводит два числа n и m, с помощью цикла вывести квадраты всех
+// let sum = 0;
+// let n = 0;
+// let midAr;
+// let i;
+// for ( i = 0; n !==null; ++i ){
+//     n = prompt ( `Number?` );
+//     if ( n !== null ){
+//     n = +n
+//     }
+//     sum = sum + n;
+// }
+// i--;
+// midAr = sum / i;
+// alert ( midAr );
+
+// Задача 1. Решение 2.
+
+// let sum = 0;
+// let number = 0;
+// let arithmeticMean;
+// let i = 0;
+// while ( true ) {
+// number = prompt ( `Number?` );
+// if ( number !== null ) { 
+// number = +number;
+// sum = sum + number;
+// i++;
+// } else {break;}
+// }
+// arithmeticMean = sum/i;
+// alert(arithmeticMean);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Задача 2.
+
+// Пользователь вводит два числа n и m, с помощью цикла вывести квадраты всех
 // чисел от n до m.
 // Например:
 // Ввели число 2
 // Ввели число 5
 // выводим: 2*2 потом 3*3 потом 4*4 потом 5*5
 
-// 3. Написать программу, в которой пользователя просят ввести числа 5 раз. Причем
+// Задача 2. Решение.
+
+// let n = +prompt(`min namber`);
+// let m = +prompt(`max namber`);
+// let cuadr;
+// for(;n<=m;n++){
+// cuadr= n*n;
+// console.log(cuadr);
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Задача 3.
+
+// Написать программу, в которой пользователя просят ввести числа 5 раз. Причем
 // каждое последующее число должно быть больше предыдущего, иначе вывести
 // ошибку.
 
-// let num1 = +prompt (`Enter 1 number`, ``);
-// let num2 = +prompt (`Enter 2 number`, ``);
-// let num3 = +prompt (`Enter 3 number`, ``);
-// let num4 = +prompt (`Enter 4 number`, ``);
-// let num5 = +prompt (`Enter 5 number`, ``);
+// Задача 3. Решение 1.
 
-// let num = +prompt (`Enter a number`);
-// let num1 = num;
-// for (let i= 5; i !== 1; i--) {
-//     +prompt (`Enter a number`);
-//     if (num > num+1) {
-//         alert (`Error!`);
-//         {break};
+// let currentNum;
+// let previousNum;
+// for (let i = 0; i < 5; i++) {
+//     if (previousNum === undefined) {
+//         previousNum = +prompt('enter a num');
+//         continue;
+//     }
+//     currentNum = +prompt('enter a num bigger than previous');
+//     if (currentNum > previousNum) {
+//         previousNum = currentNum;
+//     } else {
+//         alert('ERROR');
+//         break;
 //     }
 // }
-// console.log (`Good job!`);
 
+// Задача 3. Решение 2.
 
-
-// if (num1 < num2) {
-//     console.log ("Yes");
-// } else {console.log ("Error")};
-// if (num2 < num3) {
-//     console.log ("Yes");
-// } else {console.log ("Error")};
-// if (num3 < num4) {
-//     console.log ("Yes");
-// } else {console.log ("Error")};
-// if (num4 < num5) {
-//     console.log ("Yes");
-// } else {
-//     console.log ("Error!");
+// let i = 0;
+// let lastNumber = 0;
+// let nextNumber = 1;
+// while(i<5){
+// if(lastNumber>nextNumber){
+// alert("ERROR");
+// break;
+// }
+// lastNumber = +prompt(`Number?`);
+// i++;
+// if(i>=5){
+// break;
+// }
+// nextNumber = +prompt(`Number?`);
+// i++;
 // }
 
 ///////////////////////////////////////////////////////////////////
 
-// 4. Написать программу-заику. Пользователь вводит число. Вывести количество "за"
+// Задача 4.
+
+// Написать программу-заику. Пользователь вводит число. Вывести количество "за"
 // (в слове "заика") равное этому числу.
 // Например:
 // Вводим 3
@@ -67,9 +118,10 @@
 // Вводим 5
 // Программа выводит "зазазазазаика"
 
+// Задача 4. Решение 1.
+
 // let num = +prompt (`Введите число!`);
 // za = `за`;
-
 // for (let i = 0; num > i; i++) {
 //     if (num === i) {
 //         {break}
@@ -78,4 +130,57 @@
 //     {break}
 // }
 
+// Задача 4. Решение 2.
+
+// let number = +prompt(`how many za?`);
+// let sum = "";
+// for(let i =0;i<number;i++){
+// sum = 'za' + sum;
+// }
+// alert(`${sum}ika`);
+
 //////////////////////////////////////////////////////////////////////////
+
+// Задача про хомяков.
+
+// под диваном живет 100 хомячков
+// пользователь не знает сколько хомячков под диваном
+// напишите игру в которой пользователь вводит количество хомячков
+// которое хочет достать из под дивана
+// правила:
+// 1. пользователь не может достать хомячков больше чем их там есть 
+// (попытку засчитываем, а хомячков НЕ достаем)
+// 2. необходимо считать количество попыток за которое пользователь достанет всех хомячков
+// 3. игра заканчивается тогда когда пользователь достал всех хомячков
+// 4. если пользователь нажал отмена (в промпте) то игра заканчивает поражением
+// 5. выводить сообщения при успешном доставании хомячков, в конце игры и при отмене игры
+
+// Задача про хомяков. Решение.
+
+// let hamsters = 100;
+// let userHamsters;
+
+// for (let counter = 0;; counter++) {2
+//     userHamsters = prompt('how many hamsters ?');
+//     if (userHamsters === null) {
+//         alert('you have quit the game');
+//         break;       
+//     }
+//     userHamsters = Math.abs(userHamsters);
+
+//     // забирание хомячков
+//     if (userHamsters > hamsters) {
+//         alert(' there is no such amount of hamsters');
+//         continue;
+//     }
+
+//     hamsters -= userHamsters;
+//     alert(`you successfully got ${userHamsters} hamsters`);
+
+//     if (hamsters === 0) {
+//         alert(`You successfully got all hamsters ${counter} times`);
+//         break;
+//     }
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
