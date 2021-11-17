71f7a1dff4e9ac5986dfbcae0ebe168c950cfd94
BasilInk <vasiltsov_s_a@mail.ru>
2021-11-17 16:55:51 +0300

    proto`

---
+++ Objects/consp.js
@@ -0,0 +1,139 @@
+// function Animal (name, age) {
+//     this.name = name;
+//     this.age = age;
+// };
+
+// Animal.prototype.eat = function (food) {
+//     return `${this.name} eats ${food}`;
+// };
+
+// Animal.prototype.sleep = function () {
+//     return `${this.name} sleeps`;
+// };
+
+// const dog = new Animal (`Bobik`, 23);
+
+// console.log (dog.sleep ());
+// console.log (dog.eat (`chappi`));
+
+// function Human (name, age, job) {
+//     Animal.call (this, name ,age);
+//     this.job = job;
+// };
+
+// Human.prototype = Object.create(Animal.prototype);
+// Human.prototype.constructor = Human;
+
+// Human.prototype.learn = function (what) {
+//     return `${this.name} learns ${what}`;
+// };
+
+// Human.prototype.drink = function (what) {
+//     return `${this.name} drinks ${what} every day`;
+// };
+
+// Human.prototype.ageN = function () {
+//     return `${this.name} is ${this.age}`;
+// };
+
+// const human1 = new Human (`John`, 31, `teacher`);
+
+// console.log (human1.learn (`JS`));
+// console.log (human1.ageN ());
+// console.log (human1.sleep ());
+
+// const human2 = new Human (`Valera`, 23, `barman`);
+
+// console.log (human2.learn (`JAVA`));
+// console.log (human2.drink (`water`));
+
+// human3 = new Human (`Svetlana`, 31, `chemical`);
+
+// console.log (human3.ageN ());
+
+// console.log (human2.drink(`vodka`));
+
+// console.log (human1);
+
+/////////////////////////////////////////////////////////////////Через apply
+
+// function Programmer () {
+//     Human.apply (this, arguments);
+//     this.isProgrammer = true;
+// };
+
+///////////////////////////////////////////////////////////////Через apply 2
+
+// function Programmer (name, age, job) {
+//     Human.apply (this, [name, age, job]);
+//     this.isProgrammer = true;
+// };
+
+//////////////////////////////////////////////////////////////////Через call
+
+// function Programmer (name, age, job, isProgrammer) {
+//     Human.call(this,name,age,job);
+//     this.isProgrammer = true;
+// };
+
+// Programmer.prototype = Object.create(Human.prototype);
+// Programmer.prototype.constructor = Programmer;
+
+// const programmer1 = new Programmer (`Peter`, 33, `programmer`);
+
+// Programmer.prototype.code = function () {
+//     return `${this.name} codes JS in his ${this.age} years old. He is programmer? It's ${this.isProgrammer}!`;
+// };
+
+// const programmer2 = new Programmer (`Henry`, 23, `programmer`);
+
+// console.log (programmer1.code ());
+// console.log (programmer2.code ());
+
+// console.log (programmer1);
+
+/////////////////////////////////////////////////////////////////////////////
+
+class Animal {
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
+    };
+    eat (a) {
+        return `${this.name} eats ${a}!`
+    };
+};
+
+const animal1 = new Animal (`Tusik`, 11);
+console.log (animal1.eat (`plov`));
+
+class Human extends Animal {
+    constructor (job, age, name) {
+        super(name, age)
+        // аналог
+        // Animal.call(this, name, age)
+        this.job = job;
+    };
+    learn (technology) {
+        return `${this.name} learns ${technology}!`;
+    };
+};
+
+const h1 = new Human (`programmer`,23,`John`);
+console.log (h1.learn (`JS`));
+console.log (h1.eat (`buter`));
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+// const arr = [3,4,5,6,7,8];
+
+// const arr2 = arr.map ((item) => {
+//     return item**2
+//     }
+// );
+
+// console.log (arr2);
+
+//////////////////////////////////////////////////////////////////////////////
--- Objects/objectsHW_Classes.js
+++ Objects/objectsHW_Classes.js
@@ -11,6 +11,47 @@
 // каждой стороны треугольника должны быть меньше суммы
 // длин остальных сторон.
 
+// Задача 1. Решение
+
+// function Point (x,y) {
+//     this.x = x;
+//     this.y = y;
+// };
+
+// function Line (point1, point2) {
+//     this.point1 = point1;
+//     this.point2 = point2;
+// };
+
+// Line.prototype.getLength = function () {
+//     return Math.sqrt (Math.pow((this.point1.x - this.point2.x),2) + Math.pow((this.point1.y - this.point2.y),2));
+// };
+
+// const p1 = new Point (23,154);
+// const p2 = new Point (51,21);
+// const p3 = new Point (75,65);
+// const p4 = new Point (86,321);
+// const p5 = new Point (67,234);
+// const p6 = new Point (89,76);
+
+// const line1 = new Line (p1,p2);
+// const line2 = new Line (p3,p4);
+// const line3 = new Line (p5,p6);
+
+// function canTriangleExist (line1,line2,line3) {
+//     const line1Length = line1.getLength();
+//     const line2Length = line2.getLength();
+//     const line3Length = line3.getLength();
+
+//     return (line1Length < line2Length + line3Length 
+//         && line2Length < line1Length + line3Length 
+//         && line3Length < line1Length + line2Length);
+// };
+
+// console.log (canTriangleExist(line1,line2,line3));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // 2. Создать конструктор Car, который принимает параметры
 // model, color, price. Конструктор должен возвращать объект в
 // котором присутствуют вышеназванные поля со значениями.
@@ -18,13 +59,31 @@
 // run() который выводит сообщение вида : MODEL + “ ” +
 // COLOR + ” is running”.
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // 3. Создать конструктор Book, с помощью этого конструктора
 // создать массив из 8 книг (объектов). Каждая книга должна
 // представлять из себя объект, в котором есть три поля:
 // author, bookName, pageAmount.
 // Создать функцию getBiggerBook(), которая принимает
 // массив книг и возвращает ОДНУ книгу с наибольшим
-// количеством страниц
+// количеством страниц.
+
+// Задача 3. Решение
+
+// function Book (author,bookName,pageAmount) {
+//     this.author = author;
+//     this.bookName = bookName;
+//     this.pageAmount = pageAmount;
+//     return `${author}, ${bookName}, ${pageAmount}`;
+// };
+// console.log (Book (`kra`,`bra`,23));
+
+// const book1 = new Book (`kra`,`bra`,23);
+// console.log (book1 ());
+// function getBiggerBook () {};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // 4. Создать класс Timer, инстанс которого запускает таймер и выводит в консоль
 // прошедшее время в секундах (1 2 3 4 5 и тд)
@@ -33,3 +92,41 @@
 // С помощью Timer вы должны создать несколько таймеров и убедится в том, что они 
 // работают независимо друг от друга.
 // останавливайте таймер через некоторое время после запуска с помощью setTimeout()
+
+// Задача 4. Решение
+
+// function Timer () {
+//     this.time = 0;
+//     this.timerId = null;
+// }
+
+// Timer.prototype.start = function () {
+//     this.timerId = setInterval (() => {
+//         console.log (this.time++);
+//     },1000)
+// }
+
+// Timer.prototype.stop = function () {
+//     clearInterval (this.timerId);
+// };
+// Timer.prototype.reset = function () {
+//     this.time = 0;
+// };
+
+// const timer = new Timer ();
+
+// timer.start ();
+
+// setTimeout (() => {
+//     timer.reset ();
+// },4000);
+
+////////////////////////////// Или через Bind
+
+// setTimeout (timer.reset.bind(timer), 4000);
+
+// setTimeout (() => {
+//     timer.stop ();
+// },8000);
+
+//////////////////////////////////////////////////////////////////////////////////////////
